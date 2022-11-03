64487518200a4543dbcb50b80f10e3995e353f3a
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-11-03 11:18:30 +0100

    Update __init__.py

--- fastapi_auth/__init__.py
+++ fastapi_auth/__init__.py
@@ -1,6 +1,6 @@
 """Authentication Based API Security with FastAPI and Postgres Database or Sqlite3 Database.
 """
 
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 from fastapi_auth.endpoints import api_key_router
 from fastapi_auth.security_api_key import api_key_security
